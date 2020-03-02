f2e6817c069c5a34bcf4d4f9fe31f3eb849794b1
mixail.olesik <mixail.olesik@gmail.com>
2020-03-02 23:42:11 +0300

    V3 Z3

--- task3.js
+++ task3.js
@@ -12,3 +12,14 @@ function hasProperty(obj, prop){
 	else
 		return false
 }
+
+//Вариант 3
+function getEnumProps(obj){
+	let a = [];
+	for (i in obj){
+		if(Number.isInteger(i)){
+			a.push(i)
+		}
+	}
+	return a
+}
